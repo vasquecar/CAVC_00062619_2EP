4757a4c4f174d59afc26c8fb01b16ac71be52afa
Alfredo Vasquez <00062619@uca.edu.sv>
2020-05-31 19:11:39 -0600

    add:exam recovery 2

---
+++ Recuperacion/Preparcial/Modelo/Inventario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Preparcial.Modelo
+{
+    public class Inventario
+    {
+        //se agrego la palabra public.
+       public string idArticulo { get; }
+       public string producto { get; }
+       public string descripcion { get; }
+       public string precio { get; }
+       public string stock { get; }
+
+        public Inventario(string idArticulo, string producto, string descripcion, string precio, string stock)
+        {
+            this.idArticulo = idArticulo;
+            this.producto = producto;
+            this.descripcion = descripcion;
+            this.precio = precio;
+            this.stock = stock;
+        }
+    }
+}
